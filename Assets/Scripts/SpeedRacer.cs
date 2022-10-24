9cf5132ed31f177e9c5ee5224b18989568f90721
pjotr.surkov <pjotr.surkov@bigbank.ee>
2022-10-25 00:51:36 +0300

    Add assignment A.03.2

--- Assets/Scripts/SpeedRacer.cs
+++ Assets/Scripts/SpeedRacer.cs
@@ -5,13 +5,26 @@ using UnityEngine;
 
 public class SpeedRacer : MonoBehaviour
 {
-    string carModel = "GTR R35";
-    string engineType = "V6, Twin Turbo";
-    int carWeight = 1609;
-    int yearMade = 2009;
-    double maxAcceleration = 0.98;
-    bool isCarTypeSedan = false;
-    bool hasFrontEngine = true;
+    class Fuel
+    {
+        public int fuelLevel;
+
+        public Fuel(int amount)
+        {
+            fuelLevel = amount;
+        }
+    }
+
+    Fuel carFuel = new Fuel(100);
+
+    public string carMaker;
+    public string carModel = "GTR R35";
+    public string engineType = "V6, Twin Turbo";
+    public int carWeight = 1609;
+    public int yearMade = 2009;
+    public double maxAcceleration = 0.98;
+    public bool isCarTypeSedan = false;
+    public bool hasFrontEngine = true;
 
     int weightThreshold = 1500;
     int yearThreshold = 2009;
@@ -19,7 +32,7 @@ public class SpeedRacer : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        print($"The car model is {carModel} and the engine type is {engineType}.");
+        print($"The car model is {carModel} ({carMaker}) and the engine type is {engineType}.");
 
         CheckWeight();
 
@@ -43,7 +56,14 @@ public class SpeedRacer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-       
+        var keyIsDown = Input.GetKeyDown(KeyCode.Space);
+
+        if (keyIsDown)
+        {
+            ConsumeFuel();
+        }
+
+        CheckFuelLevel();
     }
 
     void CheckWeight()
@@ -78,6 +98,30 @@ public class SpeedRacer : MonoBehaviour
         else
         {
             return "The car is neither a sedan or has a front engine.";
+        }
+    }
+
+    void ConsumeFuel()
+    {
+        carFuel.fuelLevel = Math.Max(0, carFuel.fuelLevel - 10);
+    }
+
+    void CheckFuelLevel()
+    {
+        switch (carFuel.fuelLevel)
+        {
+            case 70:
+                print("The fuel level is nearing two-thirds.");
+                break;
+            case 50:
+                print("The fuel level is at half amount.");
+                break;
+            case 10:
+                print("Warning! Fuel level is critically low.");
+                break;
+            default:
+                print($"The fuel level is {carFuel.fuelLevel}.");
+                break;
         }
     }
 }
